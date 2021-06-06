974ce9f87d31c2f0103f3bbe3ecd298fd82674a7
roman.nikolaev <roman.nikolaev@ics-it.ru>
2021-06-06 23:09:06 +0300

    refactor anyOf

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,11 @@ import { Playground } from "./playground/Playground";
 const schema = require("./examples/example.json");
 
 render(<Playground schema={schema}></Playground>, document.getElementById("root"));
-render(
-	<div className={style.main}>
-		<JsonSchemaForm schema={schema} onSubmit={(...args) => console.log(args)} />
-		{/* <App /> */}
-		{/* <Charts /> */}
-	</div>,
-	document.getElementById("root")
-);
+// render(
+// 	<div className={style.main}>
+// 		<JsonSchemaForm schema={schema} onSubmit={(...args) => console.log(args)} />
+// 		{/* <App /> */}
+// 		{/* <Charts /> */}
+// 	</div>,
+// 	document.getElementById("root")
+// );
